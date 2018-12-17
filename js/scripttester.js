a121995423922260fee53a781a979a142c5a975b
Jessica Kaplan <jessicakaplan@Jessicas-MacBook-Air.local>
2018-12-17 12:22:25 +0100

    new ana novic

--- js/scripttester.js
+++ js/scripttester.js
@@ -10,35 +10,63 @@ function getPosts1() {
 }
 
 function showPosts1(data) {
-    console.log(data.categories);
+    console.log(data);
     /*console.log(data._embedded["wp:featuredmedia"][0].media_details.sizes.full.source.source_url);*/
     data.forEach(data => {
+        console.log(data);
         const clone = myTemplate.cloneNode(true);
         const postTitle = data.title.rendered;
         const postContent = data.content.rendered;
-        clone.querySelector(".postTitle").textContent = postTitle;
+        /*clone.querySelector(".postTitle").textContent = postTitle;
         clone.querySelector(".postContent").innerHTML = postContent;
-        clone.querySelector(".img").src = data._embedded["wp:featuredmedia"][0].source_url;
+        clone.querySelector(".img").src = data._embedded["wp:featuredmedia"][0].source_url;*/
         document.querySelector("main").appendChild(clone);
-        /*console.log(data.categories);*/
+        console.log(data.categories[0])
 
-        if (data.categories === 2) {
-            document.querySelector(".postLeft").innerHTML = data._embedded["wp:featuredmedia"]
-            [0].source_url;
+        if (data.categories[0] === 2) {
+            document.querySelector(".postLeft").src = data._embedded["wp:featuredmedia"]
+              [0].source_url;
             document.querySelector(".postLeft").innerHTML = postTitle;
             document.querySelector(".postLeft").innerHTML = postContent;
-        }
-        else (data.categories === 3){
-            document.querySelector(".postRight").innerHTML = data._embedded["wp:featuredmedia"]
-            [0].source_url;
+        } else if (data.categories[0] === 3) {
+            document.querySelector(".postRight").src = data._embedded["wp:featuredmedia"]
+              [0].source_url;
             document.querySelector(".postRight").innerHTML = postTitle;
             document.querySelector(".postRight").innerHTML = postContent;
         }
-
     });
 }
 
+/*const catID = params.get("catid");*/
 
+
+
+//categories
+/*if(catID === 2){
+	;
+}else{
+	;
+}*/
+
+/*function loadCategories() {
+    fetch(baseLink + "/categories").then(e => e.json()).then(makeCatStyling);
+}
+
+function makeCatStyling(cats) {
+    cats.forEach(cat => {
+            console.log(cat.id);
+            if (cat.id === 2) {
+            clone.querySelector(".leftSide") = data.content.rendered
+            } else{
+             main.classList.add(".postRight")
+            }
+        }
+
+    );
+}*/
+
+
+// loadCategories();
 
 
 getPosts1();
